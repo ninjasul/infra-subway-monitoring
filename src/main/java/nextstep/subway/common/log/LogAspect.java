1e49b6c1e2eff7346e2a0eeec66eb9856562000f
dev.prod(박동엽)/kakaopay <dev.prod@kakaopaycorp.com>
2021-06-21 13:02:39 +0900

    feat: step1 피드백 반영. application log 추가

--- src/main/java/nextstep/subway/common/log/LogAspect.java
+++ src/main/java/nextstep/subway/common/log/LogAspect.java
@@ -1,7 +1,6 @@
 package nextstep.subway.common.log;
 
 import static net.logstash.logback.argument.StructuredArguments.kv;
-import static nextstep.subway.common.log.Loggers.FILE_LOGGER;
 
 import java.util.Arrays;
 import java.util.stream.Collectors;
@@ -10,7 +9,8 @@ import nextstep.subway.common.util.StringUtils;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
-import org.springframework.http.ResponseEntity;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
@@ -18,28 +18,31 @@ import org.springframework.web.context.request.ServletRequestAttributes;
 @Component
 @Aspect
 public class LogAspect {
-    @Around("within(nextstep.subway..*) && "
-            + "(@annotation(org.springframework.web.bind.annotation.RequestMapping) || "
-            + "@annotation(org.springframework.web.bind.annotation.GetMapping) || "
-            + "@annotation(org.springframework.web.bind.annotation.PostMapping) || "
-            + "@annotation(org.springframework.web.bind.annotation.DeleteMapping) || "
-            + "@annotation(org.springframework.web.bind.annotation.PutMapping))")
+    private final static Logger log = LoggerFactory.getLogger(LogAspect.class);
+
+    @Around("(execution(* nextstep.subway.*.ui.AuthController.*(..)) || "
+            + "execution(* nextstep.subway.*.ui.LineController.*(..)) || "
+            + "execution(* nextstep.subway.*.ui.MemberController.*(..)))")
     public Object handle(ProceedingJoinPoint pjp) throws Throwable {
         long start = System.currentTimeMillis();
         Object returnObj = null;
         try {
-            return returnObj = pjp.proceed(pjp.getArgs());
-        } finally {
+            returnObj = pjp.proceed(pjp.getArgs());
             writeLog(pjp, returnObj, getHttpServletRequest(), System.currentTimeMillis() - start);
+            return returnObj;
+        } catch (Throwable t) {
+            writeErrorLog(getHttpServletRequest(), t);
+            throw t;
         }
     }
+
 
     private HttpServletRequest getHttpServletRequest() {
         return ((ServletRequestAttributes) RequestContextHolder.currentRequestAttributes()).getRequest();
     }
 
     private void writeLog(ProceedingJoinPoint pjp, Object returnObj, HttpServletRequest request, long elapsed) {
-        FILE_LOGGER.info("[{}][{}][{}][{}ms]: {} -> {}",
+        log.info("[{}][{}][{}][{}ms]: {} -> {}",
                 request.getRemoteHost(),
                 request.getMethod(),
                 request.getRequestURI(),
@@ -53,5 +56,13 @@ public class LogAspect {
         return Arrays.stream(pjp.getArgs())
                 .map(StringUtils::toSimplifiedIndentedJson)
                 .collect(Collectors.joining("\n"));
+    }
+
+    private void writeErrorLog(HttpServletRequest request, Throwable t) {
+        log.error(String.format("[%s][%s][%s]: ",
+                request.getRemoteHost(),
+                request.getMethod(),
+                request.getRequestURI()),
+                t);
     }
 }
